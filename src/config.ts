d8db2b90a8d5344d0ab99331792ede7c7c070ea9
tangyuan0821 <tangyuan0821@hotmail.com>
2026-01-31 11:00:42 +0800

    fix: update banner position from center to top in site configuration

--- src/config.ts
+++ src/config.ts
@@ -19,7 +19,7 @@ export const siteConfig: SiteConfig = {
 		enable:true,
 		src: "assets/images/128919932_p0_master1200.jpg", // 相对于 /src 目录。如果以 '/' 开头则相对于 /public 目录
 		
-		position: "center", // 相当于 object-position，只支持 'top'、'center'、'bottom'。默认为 'center'
+		position: "top", // 相当于 object-position，只支持 'top'、'center'、'bottom'。默认为 'center'
 		credit: {
 			enable: true, // 显示横幅图片的来源文字
 			text: "釧路の空", // 要显示的来源文字
